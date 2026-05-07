0e1eea33f85ef9897c7a650f2f1586b28d67bf96
estebanvalenccia-coder <estebanvalenccia@gmail.com>
2026-05-08 00:12:06 +0200

    Stop preloading backend before rendering app

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
-import { backendApi } from "./app/lib/backendStorage";
 
-backendApi.preload().finally(() => {
-  createRoot(document.getElementById("root")!).render(<App />);
-});
+createRoot(document.getElementById("root")!).render(<App />);
